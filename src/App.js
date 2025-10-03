817dd8b536c1d054e9ad03d2beedb72fe528ca1e
Jasmine <oatman.estates.holdings@gmail.com>
2025-10-03 08:24:37 -0400

    confused af

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React from 'react';
 import './App.css';
 import { auth } from './firebase/init';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword, 
+  signOut, 
+  onAuthStateChanged } from "firebase/auth";
 
 function App() {
+  const [user, setUser] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      console.log(user);
+      if (user) {
+        setUser(user)
+      }
+    })
+  }, []);
+
   function register() {
     createUserWithEmailAndPassword(auth, 'email@email.com', 'pswd123')
     .then((user) => {
@@ -15,18 +32,39 @@ function App() {
 
   function login() {
     signInWithEmailAndPassword(auth, 'email@email.com', 'pswd123')
-    .then((user) => {
-      console.log(user)
+    .then(({ user }) => {
+      console.log(user);
+      setUser(user);
     })
     .catch((error) => {
+      // setErrorMessage('The password is invalid or the user does not have a password.')
       console.log(error.message);
     })
   }
 
+  function logout() {
+    signOut(auth);
+    setUser({});
+    console.log('user signed out')
+  }
+
   return (
     <div className="App">
-      <button onClick={register}>Register</button>
-      <button onClick={login}>Login</button>
+
+
+      <nav>
+      <div className='nav__container'>
+         <figure>
+          <img  />
+         </figure>
+      <button className='nav__btn' onClick={login}>Login</button>
+      <button className='nav__btn nav__btn__purple' onClick={register}>Register</button>
+
+      <button className='email__initial' onClick={logout}>Logout</button>
+        {loading ? 'loading...' : user.email[0].toUpperCase()}
+      </div>
+       </nav>
+
     </div>
   );
 }
